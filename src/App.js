814e4ff66adca32b6239acab1d19daefe8cfbbda
Junick Marco Aquino <aquijun@mliph.manulife.com.ph>
2022-09-10 12:48:53 +0800

    Create Class Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Card from './components/Card';
 
-function App({title}) {
-  return (
-    <div className="header">
-      {title}
-    </div>
-  );
+class App extends React.Component {
+  // constructor
+  // this
+  // render -- required
+  render() {
+    return (
+      <div>
+        <div className="header">
+        {this.props.title}
+        </div>
+        <Card />
+      </div>
+    );
+  }
 }
+
+// function App({title}) {
+//   return (
+//     <div className="header">
+//       {title}
+//     </div>
+//   );
+// }
 
 export default App;
