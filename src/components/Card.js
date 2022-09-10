fcee4d3f5b3efec78641a9e3edfbcf2099acb9c2
Junick Marco Aquino <aquijun@mliph.manulife.com.ph>
2022-09-10 12:54:57 +0800

    Syle Components

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,10 @@ import React from 'react';
 class Card extends React.Component {
     render() {
         return (
-            <div className="github-profile">
-                <img src="https://placehold.it/75" alt="profile pic"/>
-                <div className="info">
-                    <div className="name">Name here...</div>
+            <div className="github-profile" style={{ margin: '1rem' }}>
+                <img src="https://via.placeholder.com/150" alt="profile pic"/>
+                <div className="info" style={{ display: 'inline-block', marginLeft: 10 }}>
+                    <div className="name" style={{fontSize: '125%'}}>Name here...</div>
                     <div className="company">Company here...</div>
                 </div>
             </div>
