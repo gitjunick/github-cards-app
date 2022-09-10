aa7a19af69ae99f5c92872cf9295512b913f71da
Junick Marco Aquino <aquijun@mliph.manulife.com.ph>
2022-09-10 13:39:50 +0800

    Take user input using the React State object

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 class Form extends React.Component{
-    userNameInput = React.createRef();
+    state = { userName: '' };
 
     handleSubmit = (event) => {
         event.preventDefault();
-        console.log(this.userNameInput.current.value);
+        console.log(this.state.userName);
     }
 
     render() {
         return(
             <form onSubmit={this.handleSubmit}>
-                <input type="text" placeholder="GitHub username" ref={this.userNameInput} required/>
+                <input type="text" value={this.state.userName} onChange={event => this.setState({ userName: event.target.value })} placeholder="GitHub username" required/>
                 <button>Add card</button>
             </form>
         );
