077a530e78f19341aa689953ac41591963f15662
Junick Marco Aquino <aquijun@mliph.manulife.com.ph>
2022-09-10 13:34:58 +0800

    Take user input using createRef

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,6 @@ import React from 'react';
 class Card extends React.Component {
     render() {
         const profile = this.props;
-        console.log(profile);
 
         return (
             <div className="github-profile" style={{ margin: '1rem' }}>
